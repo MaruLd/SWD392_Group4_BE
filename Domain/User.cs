451e740224d72da829bb6f7f82ca39dfc4ba1578
Johnnymc2001 <37579854+Johnnymc2001@users.noreply.github.com>
2022-05-30 07:57:05 +0700

    Add TicketUser

--- Domain/User.cs
+++ Domain/User.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Identity;
 
 namespace Domain;
@@ -7,7 +8,9 @@ namespace Domain;
 public class User : IdentityUser<Guid>
 {
 	public string DisplayName { get; set; }
-	
-	public int InventoryId { get; set; }
+
+	// public int InventoryId { get; set; }
+	[JsonIgnore]
+	public virtual ICollection<Ticket> Tickets { get; set; }
 	public DateTime CreatedDate { get; set; } = DateTime.Now;
 }
