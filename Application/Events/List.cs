275a187f9c65a6bcfa536cb096b551399801066c
Maru <accnumberthree@gmail.com>
2022-06-01 05:39:50 +0700

    Small changes

--- Application/Events/List.cs
+++ Application/Events/List.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Application.Core;
 using Application.Events.DTOs;
 using Application.Services;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -14,29 +16,34 @@ using Persistence.Repositories;
 
 namespace Application.Events
 {
-	public class List
-	{
+  public class List
+  {
 
-		public class Query : IRequest<Result<List<EventDTO>>>
-		{
-			public ListEventDTO dto { get; set; }
-		}
+    public class Query : IRequest<Result<List<EventDTO>>>
+    {
+      public ListEventDTO dto { get; set; }
+    }
 
-		public class Handler : IRequestHandler<Query, Result<List<EventDTO>>>
-		{
-			private readonly DataContext _context;
-			private readonly EventService _eventService;
+    public class Handler : IRequestHandler<Query, Result<List<EventDTO>>>
+    {
+      private readonly DataContext _context;
+      private readonly EventService _eventService;
+      private readonly IMapper _mapper;
 
-			public Handler(DataContext context, EventService eventService)
-			{
-				_context = context;
-				_eventService = eventService;
-			}
+      public Handler(DataContext context, IMapper mapper, EventService eventService)
+      {
+        _mapper = mapper;
+        _context = context;
+        _eventService = eventService;
+      }
 
-			public async Task<Result<List<EventDTO>>> Handle(Query request, CancellationToken cancellationToken)
-			{
-				return Result<List<EventDTO>>.Success(await _eventService.Get(request.dto));
-			}
-		}
-	}
+      public async Task<Result<List<EventDTO>>> Handle(Query request, CancellationToken cancellationToken)
+      {
+        var Events = await _context.Event.ProjectTo<EventDTO>(_mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+        return Result<List<EventDTO>>.Success(Events);
+				// return Result<List<EventDTO>>.Success(await _eventService.Get(request.dto));
+        
+      }
+    }
+  }
 }
