7a17e22597fb41bfcf00f6c87da4fdd0da1ec593
JohnnyMC <johnnymc2001@gmail.com>
2022-06-20 12:21:59 +0700

    TestFix

--- Application/Services/AWSService.cs
+++ Application/Services/AWSService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon;
 using Amazon.Runtime;
 using Amazon.S3;
 using Amazon.S3.Model;
@@ -16,22 +17,27 @@ namespace Application.Services
 		AmazonS3Client client;
 		public AWSService(IConfiguration configuration)
 		{
-			var amazonConfig = new AmazonS3Config
-			{
-				AuthenticationRegion = S3Region.APSoutheast1,
-				ForcePathStyle = true
-			};
-			// var accessKey = configuration.GetValue<string>("AWS:AccessKey");
-			var accessKey = "AKIAYFYSY4MYV6W7Z3X3";
-			// var secretKey = configuration.GetValue<string>("AWS:SecretKey");
-			var secretKey = "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";
+			fetchClient();
+		}
 
-			var credentials = new BasicAWSCredentials(accessKey, secretKey);
-			client = new AmazonS3Client(credentials, amazonConfig);
+		private void fetchClient()
+		{
+			if (client == null)
+			{
+
+				// var accessKey = configuration.GetValue<string>("AWS:AccessKey");
+				var accessKey = "AKIAYFYSY4MYV6W7Z3X3";
+				// var secretKey = configuration.GetValue<string>("AWS:SecretKey");
+				var secretKey = "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";
+
+				var credentials = new BasicAWSCredentials(accessKey, secretKey);
+				client = new AmazonS3Client(credentials, RegionEndpoint.APSoutheast1);
+			}
 		}
 
 		public async Task<String> UploadImage(IFormFile file, Guid key)
 		{
+			fetchClient();
 			TransferUtility utility = new TransferUtility(client);
 			TransferUtilityUploadRequest request = new TransferUtilityUploadRequest();
 
@@ -51,6 +57,7 @@ namespace Application.Services
 
 		public async Task<String> GetImage(string key)
 		{
+			fetchClient();
 			var listBucketResponse = await client.ListBucketsAsync();
 			var bucket = listBucketResponse.Buckets[0];
 
