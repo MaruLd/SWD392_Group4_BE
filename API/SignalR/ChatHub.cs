cc02be90aa71a34ec251d03c25409b8321a8a3ba
JohnnyMC <johnnymc2001@gmail.com>
2022-07-16 19:20:06 +0700

    Fix Connection ID

--- API/SignalR/ChatHub.cs
+++ API/SignalR/ChatHub.cs
@@ -28,20 +28,20 @@ namespace API.SignalR
 
 			var cmd = new Create.Command()
 			{
-				postid = _postConnections.GetPostIdFromConnection(httpContext.Connection.Id),
+				postid = _postConnections.GetPostIdFromConnection(Context.ConnectionId),
 				dto = new CreateCommentDTO() { Body = content }
 			};
 
 			var comment = await _mediator.Send(cmd);
-			var postId = _postConnections.GetPostIdFromConnection(httpContext.Connection.Id);
+			var postId = _postConnections.GetPostIdFromConnection(Context.ConnectionId);
 			var connections = _postConnections.GetConnectionsInPost(postId);
 
+			// await Clients.All.SendAsync("NewComment", comment.Value);
 			foreach (var c in connections)
 			{
 				var client = Clients.Client(c);
 				await client.SendAsync("NewComment", comment.Value);
 			}
-
 		}
 
 		public override async Task OnConnectedAsync()
@@ -49,7 +49,7 @@ namespace API.SignalR
 			var httpContext = Context.GetHttpContext();
 			var postId = httpContext.Request.Query["postId"];
 
-			_postConnections.AddConnection(httpContext.Connection.Id, Guid.Parse(postId));
+			_postConnections.AddConnection(Context.ConnectionId, Guid.Parse(postId));
 
 			await Clients.Caller.SendAsync("Load", "Welcome");
 
@@ -60,7 +60,7 @@ namespace API.SignalR
 		public override async Task OnDisconnectedAsync(Exception? exception)
 		{
 			var httpContext = Context.GetHttpContext();
-			_postConnections.RemoveConnection(httpContext.Connection.Id);
+			_postConnections.RemoveConnection(Context.ConnectionId);
 		}
 	}
 }
